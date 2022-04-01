d08d83d53996ba57bd0760d2155e2794afb5b1cd
DChivu <chivudan.n@gmail.com>
2022-04-01 10:16:17 +0200

    Flows updated, main starts two testcases in sequence (log-out doesn't work, need to configure back functionality for the login+shop+logout flow)

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,58 +2,35 @@ import java.net.URL;
 import java.net.MalformedURLException;
 
 import core.InitialSetup;
-import core.UiSelector;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import tests.Flows;
 
 //demo class for Browserstack
 public class Runner {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-
-        // Set your access credentials
-        caps.setCapability("browserstack.user", "danchivu_JrO3PZ");
-        caps.setCapability("browserstack.key", "7MrEyLYzpzQEJsgqrzqv");
-
-        // Set URL of the application under test
-        caps.setCapability("app", "bs://f7aaacc5e194b66bbe725acc64d14457c2bc2bfb");
-
-        // Specify device and os_version for testing
-        caps.setCapability("device", "Samsung Galaxy S20 Ultra");
-        caps.setCapability("os_version", "10.0");
-
-        // Set other BrowserStack capabilities
-        caps.setCapability("project", "FSD Carrefour automation framework");
-        caps.setCapability("build", "browserstack-build-1");
-        caps.setCapability("name", "Log-in via phone number+password");
+    String sessionName = "Sign-up + obtainAFG + deleteAccount";
+    String sessionName2 = "Log-in + Eshop Basket + logOut";
 
 
-        // Initialise the remote Webdriver using BrowserStack remote URL
-        // and desired capabilities defined above
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
-                new URL("http://hub.browserstack.com/wd/hub"), caps);
-                InitialSetup.allowPolicies(driver);
-        WebDriverWait wait = new WebDriverWait(driver, 5000);
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/btn_account\")").click();
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/btn_login_register\")").click();
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/et_user_id\")").sendKeys("0722812150");
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/btn_continue\")").click();
-       // driver.findElementByAndroidUIAutomator(UiSelector.className("android.widget.TextView")).sendKeys("123987";
-        wait.until(ExpectedConditions.elementToBeClickable((driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/et_password\")"))));
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/et_password\")").sendKeys("123987");
-        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.carrefourpay.ro:id/btn_login\")").click();
+       DesiredCapabilities caps = InitialSetup.initiateCapabilities(sessionName);
+       AndroidDriver<AndroidElement> driver = InitialSetup.initiateDriver(caps);
+       InitialSetup.allowPolicies(driver);
+       Flows.signUp(driver,caps);
+       Flows.obtainAFGCode(driver,caps);
+       Flows.deleteAccount(driver,caps);
+       driver.quit();
 
-assert(driver.findElementByAndroidUIAutomator("new UiSelector().className(\"android.widget.TextView\")").isDisplayed());
-        // Write your test case statements here
 
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
+
+       DesiredCapabilities caps1 = InitialSetup.initiateCapabilities(sessionName2);
+       AndroidDriver<AndroidElement> driver1 = InitialSetup.initiateDriver(caps1);
+       InitialSetup.allowPolicies(driver1);
+       Flows.login(driver1,caps1);
+       Flows.addItemToEShopBasket(driver1,caps1);
+       Flows.logOut(driver1,caps1);
+       driver1.quit();
 
     }
 }
--- src/main/java/core/InitialSetup.java
+++ src/main/java/core/InitialSetup.java
@@ -1,9 +1,42 @@
 package core;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.junit.Assert;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class InitialSetup {
+    public  static  DesiredCapabilities initiateCapabilities (String sessionName){
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        // Set your access credentials
+        caps.setCapability("browserstack.user", "danchivu_JrO3PZ");
+        caps.setCapability("browserstack.key", "7MrEyLYzpzQEJsgqrzqv");
+
+        // Set URL of the application under test
+        caps.setCapability("app", "bs://80dc4c4641d3515890aa4fd0279e7db72a95e871");
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Samsung Galaxy S20 Ultra");
+        caps.setCapability("os_version", "10.0");
+
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "FSD Carrefour automation framework");
+        caps.setCapability("build", "browserstack-build-2");
+        caps.setCapability("name", sessionName);
+
+        return caps;
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+    }
+    public static AndroidDriver<AndroidElement> initiateDriver(Capabilities caps) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://hub.browserstack.com/wd/hub"), caps);
+        return driver;
+    }
     public static void allowPolicies(AndroidDriver driver){
 
         driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.android.permissioncontroller:id/permission_allow_foreground_only_button\")").click();
